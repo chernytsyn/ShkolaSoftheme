4dc7912f624fc2eeac03f3639fe39487271b47d6
chernytsyn <chernytsyn@gmail.com>
2017-09-21 23:48:56 +0300

    -reworked hometask

--- AngularHW/Lection2_homework/my-dream-app/src/app/article-details/article-details.component.ts
+++ AngularHW/Lection2_homework/my-dream-app/src/app/article-details/article-details.component.ts
@@ -1,6 +1,13 @@
 import { Component} from '@angular/core';
 import {FormsModule} from '@angular/forms' 
 
+
+export class Article {
+  id:number;
+  title:string;
+  shortDescription:string;
+}
+
 @Component({
   selector: 'app-article-details',
   templateUrl: './article-details.component.html',
@@ -11,20 +18,14 @@ export class ArticleDetailsComponent {
 
     color:string = 'black';
     heading:string = "";
-
-    id:number = 1;
-    title:string = 'This is Chernytsyn article';
-    shortDescription:string = 'This is shortDescription of the arctile';
+    articles: Article[] = [{
+      id : 1,
+      title :'This is Chernytsyn article',
+      shortDescription: 'This is shortDescription of the arctile'
+    }];
 
     changeColor():void {
-      if (this.color=='black')
-        {
-          this.color='gray';
-        }
-      else {
-        this.color='black'
-      }
-      }
+      this.color = this.color === 'black' ? 'gray' : 'black';
     }
  
-
+}
